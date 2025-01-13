b0e963e27430f096dcea422000773e1fa98aca56
InJRaven <kuuhaku989898@gmail.com>
2025-01-14 00:57:55 +0700

    first commit

---
+++ src/views/App.jsx
@@ -0,0 +1,17 @@
+import { Suspense } from "react";
+import { useRoutes } from "react-router-dom";
+import { AppRoutesConfig } from "../routes/AppRoutes";
+import ScrollToTop from "../utils/ScrollToTop";
+
+function App() {
+  const routes = useRoutes([...AppRoutesConfig]);
+
+  return (
+    <>
+      <ScrollToTop />
+      <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>
+    </>
+  );
+}
+
+export default App;
